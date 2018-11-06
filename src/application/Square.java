87b315e60ec39bfef01082a22390e61ed0df8e43
Berthier <berthierm@wit.edu>
2018-11-06 14:22:56 -0500

    +Board, +Square

---
+++ src/application/Square.java
@@ -0,0 +1,16 @@
+package application;
+
+public class Square {
+	int[] position;
+	String bonus;
+	
+	public Square(int[] givenPos, String bonus) {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
