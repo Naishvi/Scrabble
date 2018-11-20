735954f079fa3697c0bf068dfcd621307b6fc925
Berthier <berthierm@wit.edu>
2018-11-20 12:51:43 -0500

    new update

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -3,6 +3,7 @@ package application;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.effect.GaussianBlur;
 import javafx.scene.input.MouseEvent;
@@ -10,6 +11,11 @@ import javafx.scene.text.Text;
 
 public class MainController {
 
+    private Button btnOK = new Button();
+    private Button btnCancel = new Button();
+    private Button playTurn = new Button();
+    private Button skipTurn = new Button();
+    
     
 
 }
